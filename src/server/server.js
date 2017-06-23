6e14efa702fd04992588bda059e66ec73321c04c
thechutrain <chu.chuyalan@gmail.com>
2017-06-23 13:19:24 -0400

    made a server page

---
+++ src/server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const PORT = process.env.PORT || 3001
+const app = express()
+
+app.get('/', (req, res) => {
+	res.json({ test: 'hi' })
+})
+
+app.listen(PORT, () => {
+	console.log(`Listening on port: ${PORT}`)
+})
