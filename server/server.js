2ecb0f228d881e3af4337397a3f12a16b6caf958
thechutrain <chu.chuyalan@gmail.com>
2017-06-26 17:31:36 -0400

    fixed concurrently and added production + development versions

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 const express = require('express')
+const path = require('path')
 const PORT = process.env.PORT || 3001
 const app = express()
 
-app.get('/test', (req, res) => {
-	res.json({ test: 'hi' })
-})
+// ======= DEVELOPMENT BUILD =======
+if (process.env.NODE_ENV === 'DEV') {
+	console.log('======== DEVELOPMENT ENVIRONMENT!!!! ========')
+	app.get('/test', (req, res) => {
+		res.json({ test: 'hi' })
+	})
+	app.listen(PORT, () => {
+		console.log(`Listening on port: ${PORT}`)
+	})
+}
 
-app.listen(PORT, () => {
-	console.log(`Listening on port: ${PORT}`)
-})
+// ======= PRODUCTION BUILD =========
+if (process.env.NODE_ENV === 'PROD') {
+	console.log('======== PRODUCTION ENVIRONMENT!!!! ========')
+	app.use(express.static(path.join(__dirname, '../', 'build')))
+	app.get('/test', (req, res) => {
+		res.json({ test: 'hello' })
+	})
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '../', '/build/index.html'))
+	})
+	app.listen(PORT, () => {
+		console.log(`Listening on port: ${PORT}`)
+	})
+}
