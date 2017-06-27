e0b3bf59addf572d966e8a03d171e06fadb691d9
thechutrain <chu.chuyalan@gmail.com>
2017-06-27 10:04:49 -0400

    restructured server

--- server/server.js
+++ server/server.js
@@ -3,28 +3,28 @@ const path = require('path')
 const PORT = process.env.PORT || 3001
 const app = express()
 
+// ======= Add middlerware =======
+
 // ======= DEVELOPMENT BUILD =======
 if (process.env.NODE_ENV === 'DEV') {
 	console.log('======== DEVELOPMENT ENVIRONMENT!!!! ========')
-	app.get('/test', (req, res) => {
-		res.json({ test: 'hi franklin!!!' })
-	})
-	app.listen(PORT, () => {
-		console.log(`Listening on port: ${PORT}`)
-	})
 }
 
 // ======= PRODUCTION BUILD =========
 if (process.env.NODE_ENV === 'PROD') {
 	console.log('======== PRODUCTION ENVIRONMENT!!!! ========')
 	app.use(express.static(path.join(__dirname, '../', 'build')))
-	app.get('/test', (req, res) => {
-		res.json({ test: 'hello' })
-	})
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, '../', '/build/index.html'))
 	})
-	app.listen(PORT, () => {
-		console.log(`Listening on port: ${PORT}`)
-	})
 }
+
+// ======== API ROUTES HERE ========
+app.get('/test', (req, res) => {
+	res.json({ test: 'hi franklin!!!' })
+})
+
+// ======== Start Server ========
+app.listen(PORT, () => {
+	console.log(`Listening on port: ${PORT}`)
+})
